b50fe2803dc2625ad0f22e74240202ffc1d34207
Devikapr <devikapr4@gmail.com>
2024-10-23 13:56:16 +0530

    Updated:Functions defined globally

--- Basics/function_definitions.dart
+++ Basics/function_definitions.dart
@@ -3,45 +3,44 @@ void main() {
   
   /**Type 1
    * Function with no arguments and no return type**/
-  void myData(){
-    print("Name : Devika\nCourse : Flutter\nCompany : Techminds\n");
-  }
-  //Function call
   myData();
   
   /**Type 2
    * Function with return type and no arguments**/
-  String mergeStrings(){
-    String firstString = "Hello";
-    String secondString = "World";
-    String mergedString = firstString+secondString;
-    return mergedString;
-  }
-  
-  //Function call
+ 
   String getMergedString = mergeStrings();
   print("Merged string is : $getMergedString\n");
   
   /**Type 3
    * Function with parameters but no return type**/
-  void setEmployeeData({String? empName,int? empId,String company = "NDZ"}){
-    print("Employee Name : $empName\nEmployee ID : $empId\nCompany Name : $company\n");
-  }
-  
-  //Function call
+
   setEmployeeData(empName: "Amala",empId: 100);
   setEmployeeData(empName: "Amala",empId: 100,company: "Infosys");
   setEmployeeData(company: "XYZ");
   
+  
   /**Type 4
    * Function with both return type and parameters**/
-  int findSum({required int x,required int y}){
-    int result = x + y;
-    return result;
-  }
   
-  //Function call
   int result = findSum(x: 10,y: 20);
   print("Sum is : $result");
   
 }
+
+void myData(){
+    print("Name : Devika\nCourse : Flutter\nCompany : Techminds\n");
+  }
+String mergeStrings(){
+    String firstString = "Hello";
+    String secondString = "World";
+    String mergedString = firstString+secondString;
+    return mergedString;
+  }
+void setEmployeeData({String? empName,int? empId,String company = "NDZ"}){
+    print("Employee Name : $empName\nEmployee ID : $empId\nCompany Name : $company\n");
+  }
+
+  int findSum({required int x,required int y}){
+    int result = x + y;
+    return result;
+  }
